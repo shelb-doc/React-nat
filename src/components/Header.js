eef3d9fb96308c5032895d6e4ca7aefcbf678ec4
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2021-06-04 13:33:17 -0400

    made the project more unique

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import { Heading } from '@chakra-ui/react';
 function Header() {
   return (
     <header className="container mx-auto">
-      <Heading as="h1">Mimikyu Records</Heading>
+      <Heading as="h1">&#128008; Mimikyu Records &#127926;</Heading>
     </header>
   );
 }
