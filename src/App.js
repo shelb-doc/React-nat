c001562e907f439caed837c0567d32f381d67f8b
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2021-06-04 13:14:59 -0400

    added header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { Box, ChakraProvider, theme } from '@chakra-ui/react';
+import { ChakraProvider, theme } from '@chakra-ui/react';
 import React from 'react';
+import { Header } from 'components';
 import './index.css';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box>Hello Mimikyu</Box>
+      <Header />
     </ChakraProvider>
   );
 }
